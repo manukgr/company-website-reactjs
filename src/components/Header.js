41dc1a33559966909a25bae9e13c028716765933
MandeepSingh <mandeep.singh@barco.com>
2023-09-08 20:12:04 +0530

    text chng

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header>
       <video src="./video.mp4" loop autoPlay muted></video>
       <h1>Super Soft Technologies</h1>
-    <h3>One stop solution for your software products with AI power 
+    <h3 className="text-white">One stop solution for your software products with AI power 
         Free one month demo
     </h3>
       {/* // <div className="row">
