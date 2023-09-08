0ed41f138d109fa543480409c1b401c164fea1f9
manukgr <manumandeep1818@gmail.com>
2023-09-08 16:55:17 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,13 +7,16 @@ function Services() {
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Web Development" img="card1.png" text="Morbi eget neque risus. Duis erat quam, porta quis enim id, venenatis blandit nunc. " />
+                        <Card title="Web Development" img="card1.png" text="One stop solution for mobile and web development with AI-enabled technologies" />
+                    </div>
+                    // <div className="col-md-4 mb-2">
+                    // <Card title="E-Commerce Services" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
+                    // </div>
+                    <div className="col-md-4 mb-2">
+                    <Card title="Cyber Security" img="card3.png" text="Fully automated AI-powered cyber security testing" />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="E-Commerce Services" img="card2.png" text="Maecenas dictum efficitur felis non gravida. Vestibulum vitae ante luctus, accumsan mi vitae, pretium metus." />
-                    </div>
-                    <div className="col-md-4 mb-2">
-                    <Card title="Cyber Security" img="card3.png" text="Phasellus suscipit nibh at nisi finibus vestibulum sit amet vitae massa. Suspendisse non est eget elit pulvinar consectetur nec non sapien." />
+                        <Card title="Software Automated Testing" img="card2.png" text="Test automation powered by AI/ML. Increase automation speed up to 80% with our AI expert. Add AI power to your existing frameworks or just clone our frameworks for free." />
                     </div>
                 </div>
             </div>
